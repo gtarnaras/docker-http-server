7aedfda2bee315a3a15316ad410b21a125801803
George Tarnaras <george.tarnaras@automationlogic.com>
2021-02-17 00:08:44 +0000

    Fix healthckeck

--- healthcheck.py
+++ healthcheck.py
@@ -5,7 +5,7 @@ endpoint_check = "http://0.0.0.0:8080"
 try:
     resp = requests.head(endpoint_check)
     print(resp.status_code)
-    sys.exit(0)
+    sys.exit(0) # return code 0 = healthy
 except requests.ConnectionError:
     print("failed to connect")
-    sys.exit(0)
+    sys.exit(1) # return code 1 = unhealthy
